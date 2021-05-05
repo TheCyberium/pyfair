41d8a47fb96c6ef90f9454945d93c091d1bc470e
Cyberium <71754582+TheCyberium@users.noreply.github.com>
2021-05-05 16:12:51 +0200

    rands

--- pyfair/report/base_report.py
+++ pyfair/report/base_report.py
@@ -29,7 +29,7 @@ class FairBaseReport(object):
     instantiated on its own.
 
     """
-    def __init__(self, currency_prefix='$'):
+    def __init__(self, currency_prefix='R'):
         # Add formatting strings
         self._currency_prefix = currency_prefix
         self._model_or_models = None
